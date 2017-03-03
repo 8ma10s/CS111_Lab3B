46dda65e8c2d5114d7343799a62782892c4f638c
Yamato Sasaki <yamatosasaki14@gmail.com>
2017-03-03 05:43:53 -0800

    finish part 1

--- lab3b.py
+++ lab3b.py
@@ -1,6 +1,19 @@
 import csv
 
-f1=open('super.csv', "r")
-file1 = csv.reader(f1)
-for row in file1:
-    print row
+csvNames = ("super.csv","group.csv","bitmap.csv","inode.csv","directory.csv","indirect.csv")
+cf = []
+for filename in csvNames:
+    f=open(filename, "r")
+    cf.append(csv.reader(f))
+
+#PART 1 : UNALLOCATED INODES
+
+freeBlock = set([])
+for bitmap in cf[2]:
+    if (int(bitmap[0], 16) % 2 ) == 1:
+        freeBlock.add(int(bitmap[1], 10))
+
+for iNode in cf[3]:
+    for i in range(11,26):
+        if int(iNode[i], 16) in freeBlock:
+            print "UNALLOCATED BLOCK < " ,int(iNode[i],16) ,  " > REFERENCED BY INODE < " , iNode[0] , " > ENTRY < ",i - 11, " >"
