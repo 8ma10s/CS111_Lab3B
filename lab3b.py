b04a4a2816a35d852221ace54b5ff3800c66dd7c
Yamato Sasaki <yamatosasaki14@gmail.com>
2017-03-03 12:12:30 -0800

    Add part 3

--- lab3b.py
+++ lab3b.py
@@ -15,17 +15,20 @@ for bitmap in cf[2]:
 
 usedBlock = {}
 dupBlock = {}
+listedInode = set()
 for iNode in cf[3]:
+    iNodeNum = int(iNode[0])
+    listedInode.add(iNodeNum)
     for i in range(11,26):
         temp = int(iNode[i],16)
         if temp in usedBlock:
             if temp not in dupBlock:
                 dupBlock[temp] = [usedBlock[temp]]
-            dupBlock[temp].append([iNode[0],i-11])
+            dupBlock[temp].append([iNodeNum,i-11])
         elif temp != 0:
-            usedBlock[temp] = [iNode[0],i-11]
+            usedBlock[temp] = [iNodeNum,i-11]
         if temp in freeBlock:
-            print "UNALLOCATED BLOCK <" ,temp ,  "> REFERENCED BY INODE <" , iNode[0] , "> ENTRY <",i - 11, ">"
+            print "UNALLOCATED BLOCK <" ,temp ,  "> REFERENCED BY INODE <" , iNodeNum , "> ENTRY <",i - 11, ">"
 
 #PART 2: DUPLICATELY ALLOCATED BLOCK
 
@@ -34,3 +37,22 @@ for k, v in dupBlock.items():
     for pair in v:
         print "INODE <", pair[0], "> ENTRY <", pair[1], ">",
     print '\n',
+
+#PART 3: UNALLOCATED INODE
+
+dirInode = {}
+for dirEntry in cf[4]:
+    temp = dirEntry[4]
+    if temp not in dirInode:
+        dirInode[temp] = [[int(dirEntry[0]),int(dirEntry[1])]]
+    else:
+        dirInode[temp].append([int(dirEntry[0]),int(dirEntry[1])])
+
+for k,v in dirInode.items():
+    if int(k) not in listedInode:
+        print "UNALLOCATED INODE <", k, "> REFERENCED BY",
+        for i in v:
+            print "DIRECTORY <", i[0], "> ENTRY <", i[1], ">",
+        print '\n',
+
+#PART 4:
