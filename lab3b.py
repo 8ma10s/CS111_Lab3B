87168c6d161e3e41cbfa400fb4487aa9509985a7
Yamato Sasaki <yamatosasaki14@gmail.com>
2017-03-03 17:02:20 -0800

    add part 4

--- lab3b.py
+++ lab3b.py
@@ -6,19 +6,47 @@ for filename in csvNames:
     f=open(filename, "r")
     cf.append(csv.reader(f))
 
+
+#INITIAL SETUP
+sb = [0] * 9
+for row in cf[0]:
+    for i in range(9):
+        if i == 0:
+            sb[i] = int(row[i], 16)
+        else:
+            sb[i] = int(row[i])
+
+gd = {}
+index=0
+bitmapBlock = set()
+inodeBlock = set()
+for temp in cf[1]:
+    gd[index] = temp
+    bitmapBlock.add(int(temp[5],16))
+    inodeBlock.add(int(temp[4],16))
+    index += 1
+
 #PART 1 : UNALLOCATED INODES
 
 freeBlock = set()
+freeInode = set()
 for bitmap in cf[2]:
-    if (int(bitmap[0], 16) % 2 ) == 1:
-        freeBlock.add(int(bitmap[1], 10))
-
+    
+    if int(bitmap[0],16) in bitmapBlock:
+        freeBlock.add(int(bitmap[1]))
+    elif int(bitmap[0],16) in inodeBlock:
+        freeInode.add(int(bitmap[1]))
 usedBlock = {}
 dupBlock = {}
 listedInode = set()
+allocatedInode = set()
+linkCount = {}
 for iNode in cf[3]:
     iNodeNum = int(iNode[0])
+linkCount[iNodeNum] = int(iNode[5])
     listedInode.add(iNodeNum)
+    if int(iNode[10]) != 0:
+        allocatedInode.add(iNodeNum)
     for i in range(11,26):
         temp = int(iNode[i],16)
         if temp in usedBlock:
@@ -55,4 +83,11 @@ for k,v in dirInode.items():
             print "DIRECTORY <", i[0], "> ENTRY <", i[1], ">",
         print '\n',
 
-#PART 4:
+#PART 4: MISSING INODE
+
+tInodes = int(sb[1])
+for i in range(11,tInodes):
+    if i not in allocatedInode and i not in freeInode:
+        group = (i-1) / int(sb[6])
+        block = int(gd[group][4],16)
+        print "MISSING INODE <", i, "> SHOULD BE IN FREE LIST <", block, ">",
